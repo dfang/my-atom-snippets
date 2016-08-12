9e2472672c2d92a98f912a2390fff6fe85542286
fang duan <df1228@gmail.com>
2016-08-12 11:33:24 +0800

    init commit

---
+++ spec/my-atom-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MyAtomSnippetsView from '../lib/my-atom-snippets-view';
+
+describe('MyAtomSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
